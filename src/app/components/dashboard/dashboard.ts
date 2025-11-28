c89cae68980ff569c70060bddfd862f2df1bfadc
alison <adevpr11@gmail.com>
2025-11-28 12:46:45 -0300

    feat: adiciona ao componente dashboard método logout e atribui nome do usuario autenticado ao template

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
 
+  usuario = signal<string>('');
+
+  router = inject(Router);
+
+  ngOnInit(){
+    this.atribuirUsuarioAutenticado();
+  }
+
+  logout(){
+
+    if(confirm('Deseja realmente sair do sistema?')){
+
+      this.limparSessao();
+
+      this.router.navigate(['autenticar-usuario']);
+
+    }
+  }
+
+  private atribuirUsuarioAutenticado(){
+    const usuarioAutenticado = this.getUsuarioAutenticado();
+    this.usuario.set(usuarioAutenticado?.nome);
+  }
+
+  private getUsuarioAutenticado(){
+    return JSON.parse(sessionStorage.getItem('usuario') as string) ;
+  }
+
+  private limparSessao(){
+    sessionStorage.clear();
+  }
 }
