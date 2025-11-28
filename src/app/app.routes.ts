383f3e6d067583a32b9bd493d9c0cb66f455937f
alison <adevpr11@gmail.com>
2025-11-28 15:28:07 -0300

    feat: configura authguard na rota do componente dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { AutenticarUsuario } from './components/autenticar-usuario/autenticar-usuario';
 import { CriarUsuario } from './components/criar-usuario/criar-usuario';
 import { Dashboard } from './components/dashboard/dashboard';
+import { AuthGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {path: 'autenticar-usuario', component: AutenticarUsuario},
   {path: 'criar-usuario', component: CriarUsuario},
-  {path: 'dashboard', component: Dashboard},
+  {path: 'dashboard', component: Dashboard, canActivate: [AuthGuard]},
   {path: '', redirectTo:'autenticar-usuario',pathMatch: 'full'}
 ];
