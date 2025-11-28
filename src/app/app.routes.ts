be0228131413fff702b7c93ced510065f2d681f9
alison <adevpr11@gmail.com>
2025-11-28 12:07:52 -0300

    feat: adiciona rota para o componente dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AutenticarUsuario } from './components/autenticar-usuario/autenticar-usuario';
 import { CriarUsuario } from './components/criar-usuario/criar-usuario';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
   {path: 'autenticar-usuario', component: AutenticarUsuario},
   {path: 'criar-usuario', component: CriarUsuario},
+  {path: 'dashboard', component: Dashboard},
   {path: '', redirectTo:'autenticar-usuario',pathMatch: 'full'}
 ];
