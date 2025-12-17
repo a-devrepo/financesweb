bd333ac427e9558c2d2f2ef6da3569ffb9e43252
alison <adevpr11@gmail.com>
2025-12-17 11:59:51 -0300

    feat: implementa template e lógica para criar o usuário

--- src/app/components/criar-usuario/criar-usuario.ts
+++ src/app/components/criar-usuario/criar-usuario.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-criar-usuario',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './criar-usuario.html',
   styleUrl: './criar-usuario.css',
 })
 export class CriarUsuario {
 
+  private httpClient = inject(HttpClient);
+
+  mensagemErro = signal<string>('');
+  mensagemSucesso = signal<string>('');
+
+  formulario = new FormGroup({
+    nome: new FormControl('',[Validators.required,Validators.minLength(6)]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    senha : new FormControl('', [Validators.required, this.obterRegraDeValidacaoDaSenha()]),
+    senhaConfirmacao: new FormControl('',[Validators.required]),
+  })
+
+  private obterRegraDeValidacaoDaSenha(): ValidatorFn{
+    return Validators
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._#^+=-])[A-Za-z\d@$!%*?&._#^+=-]{8,}$/);
+  }
+
+  criarConta(){
+
+    const novoUsuario = this.formulario.getRawValue();
+
+    this.httpClient.post('http://localhost:8082/api/v1/usuarios/criar', novoUsuario)
+    .subscribe(
+      {
+        next: (data) => {
+          this.mensagemSucesso.set('Usuário cadastrado com sucesso!');
+          this.formulario.reset();
+        },
+        error:(e) => {
+          this.mensagemErro.set(e.error);
+        }
+      }
+    )
+  }
 }
